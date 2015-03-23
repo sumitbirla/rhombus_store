e4610b115c646b7ce26379d0631cb2b9c0593d64
Sumit Birla <sbirla@tampahost.net>
2015-03-23 16:22:07 +0100

    label fix

--- app/controllers/admin/store/easy_post_controller.rb
+++ app/controllers/admin/store/easy_post_controller.rb
@@ -66,7 +66,7 @@ class Admin::Store::EasyPostController < Admin::BaseController
     debug_str = "<pre>"
     
     @shipments.each do |shp|
-      debug_str += "\n" + shp.to_s + "\t" + shp.recipient_name.ljust(20) + (shp.recipient_city + ", " + shp.recipient_state).ljust(20)
+      debug_str += "\n" + shp.to_s + "\t" + shp.recipient_name + "\t" + shp.recipient_city + "\t" + shp.recipient_state
       
       shp.assign_attributes(
         packaging_type: @shipment_params.packaging_type,
@@ -82,7 +82,7 @@ class Admin::Store::EasyPostController < Admin::BaseController
       begin
         ep_shipment = create_ep_shipment(shp)
         selected_rate = ep_shipment.lowest_rate(carriers = [@shipment_params.carrier])
-        debug_str += ":  " + selected_rate[:carrier] + " " + selected_rate[:service] + " " + selected_rate[:rate] 
+        debug_str += ":\t" + selected_rate[:carrier] + " " + selected_rate[:service] + " " + selected_rate[:rate] 
         #next
         reply = ep_shipment.buy(rate: selected_rate)
         
