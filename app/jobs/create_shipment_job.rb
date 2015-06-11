a14dcab2073fb7ca48bb6c9d6f52637bb51fd910
Sumit Birla <sbirla@tampahost.net>
2015-06-10 22:26:17 -0400

    create shipment fix

--- app/jobs/create_shipment_job.rb
+++ app/jobs/create_shipment_job.rb
@@ -2,7 +2,7 @@ class CreateShipmentJob < ActiveJob::Base
   queue_as :default
 
   def perform(order_id)
-    order = Order.find_by(order_id)
+    order = Order.find(order_id)
     return if order.nil?
     
     order.create_shipment(nil)
