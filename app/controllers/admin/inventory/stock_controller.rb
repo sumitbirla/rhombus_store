6a4549b858b56db0c4012b1c3503eca002f52deb
Sumit Birla <sbirla@tampahost.net>
2015-06-19 20:53:13 +0200

    inventory udpate

--- app/controllers/admin/inventory/stock_controller.rb
+++ app/controllers/admin/inventory/stock_controller.rb
@@ -1,7 +1,8 @@
 class Admin::Inventory::StockController < Admin::BaseController
 
   def index
-    @products = Product.all.includes(:supplier).order('sku')
+    @products = Product.all.includes(:supplier).where(active: true).order('sku')
+    @supplier_products = AffiliateProduct.where(affiliate_id: @products.map { |x| x.primary_supplier_id })
   end
 
 end
