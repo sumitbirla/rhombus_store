2accecfdb9be780f241bc15d094159491f5a4967
Sumit Birla <sbirla@tampahost.net>
2015-10-24 10:32:25 -0400

    ui updates

---
+++ app/helpers/daily_deal_helper.rb
@@ -0,0 +1,21 @@
+module DailyDealHelper
+  
+  def deal_status(deal)
+    css = "label-default"
+    text = "pending"
+    
+		if deal.number_sold > deal.max_sales
+      css = "label-important"
+      text = "sold out"
+    elsif deal.start_time < DateTime.now && deal.end_time > DateTime.now 
+      css = "label-success"
+      text = "active"
+    elsif deal.end_time < DateTime.now
+      css = "label-warning"
+      text = "expired"
+    end
+    
+    "<span class='label #{css}'>#{text}</span>".html_safe
+  end
+  
+end
