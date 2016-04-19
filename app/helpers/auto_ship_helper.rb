4f31c37d5c0858f232109d35bf87b222593b4d97
Sumit Birla <sbirla@tampahost.net>
2016-04-19 19:07:53 +0200

    autoship status helper

--- app/helpers/auto_ship_helper.rb
+++ app/helpers/auto_ship_helper.rb
@@ -2,12 +2,10 @@ module AutoShipHelper
   
   def auto_ship_status(asi)
     
-    css = "label-info"
+    css = "label-danger"
     
     if asi.status == 'active'
       css = 'label-success'
-    elsif asi.status == 'billing_failed'
-      css = 'label-danger'
     end
 
     "<span class='label #{css}'>#{asi.status}</span>".html_safe
