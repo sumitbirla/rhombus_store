9c5fd56c334f6eb6641c731553f102e908ee0c08
Sumit Birla <sbirla@tampahost.net>
2016-08-17 06:29:26 -0400

    affiliate_products

--- app/controllers/affiliate_products_controller.rb
+++ app/controllers/affiliate_products_controller.rb
@@ -2,7 +2,12 @@ class AffiliateProductsController < ApplicationController
   
   # GET /ap/:affiliate-slug
   def index
-    @affiliate_products = StoreCache.all_affiliate_products(params[:affiliate_slug]).paginate(page: params[:page], per_page: params[:per_page])
+    aff = Affiliate.find_by(slug: params[:affiliate_slug])
+    @affiliate_products = AffiliateProduct.joins(:product)
+                    .includes(:product, [product: :pictures])
+                    .where(affiliate_id: aff.id)
+                    .where("store_products.active=1 AND store_products.hidden=0 AND store_products.affiliate_only=1")
+                    .paginate(page: params[:page], per_page: params[:per_page])
   end
   
   # GET /ap/:affiliate-slug/:product-slug
