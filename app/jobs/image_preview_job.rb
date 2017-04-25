a36ec19f152becd21205f5214f7fb7bbeff65fef
Sumit Birla <sbirla@tampahost.net>
2017-04-25 21:39:24 +0200

    image preview

--- app/jobs/image_preview_job.rb
+++ app/jobs/image_preview_job.rb
@@ -34,7 +34,7 @@ class ImagePreviewJob < ActiveJob::Base
     buffer = buffer.composite(resized_dog, Magick::ForgetGravity, elem.width * i.start_x_percent / 100.0, elem.height * i.start_y_percent / 100.0, Magick::OverCompositeOp)
 
     # copy backfill (with dog on it) to the main product image
-    output_file = "/previews/" + i.uploaded_file.split("/").last
+    output_file = "/previews/" + SecureRandom.hex(6) + ".jpg"
     final = bg.composite(buffer, elem.left, elem.top, Magick::OverCompositeOp)
     final.write(static_files_path + output_file)
     
