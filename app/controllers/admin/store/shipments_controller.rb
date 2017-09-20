4dc329fa9e6ed4a0477d306d42c8d9f6324176a9
Sumit Birla <sbirla@tampahost.net>
2017-09-20 16:41:23 +0200

    shipment fix

--- app/controllers/admin/store/shipments_controller.rb
+++ app/controllers/admin/store/shipments_controller.rb
@@ -14,7 +14,7 @@ class Admin::Store::ShipmentsController < Admin::BaseController
   def index
     q = params[:q]
     s = Shipment.includes(:order, :items, :inventory_transaction, [items: :order_item]).order(sort_column + " " + sort_direction)
-    s = s.where("recipient_name LIKE '%#{q}%' OR recipient_company LIKE '%#{q}%' OR recipient_city LIKE '%#{q}%'")
+    s = s.where("recipient_name LIKE '%#{q}%' OR recipient_company LIKE '%#{q}%' OR recipient_city LIKE '%#{q}%'") unless q.blank?
     s = s.where("store_orders.user_id = ?", params[:user_id]) unless params[:user_id].blank?
     s = s.where("store_orders.affiliate_id = ?", params[:affiliate_id]) unless params[:affiliate_id].blank?
     s = s.where(carrier: params[:carrier]) unless params[:carrier].blank?
