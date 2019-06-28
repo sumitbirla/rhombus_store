dd83f732d5e81e02103e202affb0b41ae4a73f20
Sumit Birla <sbirla@tampahost.net>
2019-06-28 22:00:52 +0200

    invoice images update

--- app/controllers/admin/store/invoice_images_controller.rb
+++ app/controllers/admin/store/invoice_images_controller.rb
@@ -2,6 +2,6 @@ class Admin::Store::InvoiceImagesController < Admin::BaseController
   def index
     authorize InvoiceImage.new
     @invoice_image = InvoiceImage.find_by(id: 1)
-    @invoice_image = InvoiceImage.create if @invoice_image.nil?
+    @invoice_image = InvoiceImage.create(domain_id: Rails.configuration.domain_id) if @invoice_image.nil?
   end
 end
