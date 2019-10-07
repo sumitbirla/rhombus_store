485bfa293dbeaae5f3d6ab11e38fbca171685136
Sumit Birla <sbirla@tampahost.net>
2019-10-07 20:54:12 +0200

    manifests update

--- app/controllers/admin/store/manifests_controller.rb
+++ app/controllers/admin/store/manifests_controller.rb
@@ -5,10 +5,13 @@ class Admin::Store::ManifestsController < Admin::BaseController
   def index
     authorize Manifest.new
     
+    aff_id = Setting.get(Rails.configuration.domain_id, :shipping, "Affiliate ID")
+    
     sql = <<-EOF
       select carrier, count(*)
       from store_shipments
-      where status = 'shipped'
+      where fulfilled_by_id = #{aff_id}
+      and status = 'shipped'
       and ship_date = '#{Date.today}'
       and manifest_id IS NULL
       and carrier <> ''
@@ -25,14 +28,16 @@ class Admin::Store::ManifestsController < Admin::BaseController
   
   def create
     manifest = authorize Manifest.new(manifest_params)
+    aff_id = Setting.get(Rails.configuration.domain_id, :shipping, "Affiliate ID")
+    
     # EasyPost.api_key = Cache.setting(Rails.configuration.domain_id, :shipping, 'EasyPost API Key')
     EasyPost.api_key = "EZAKf450411cc98a4a6eac6f7671d89a0b0cjOAdPcyWzaTCJoSIfcz8mg"
-    shipments = Shipment.where(ship_date: manifest.day, carrier: manifest.carrier, status: :shipped, manifest_id: nil)
+    shipments = Shipment.where(fulfilled_by_id: aff_id, ship_date: manifest.day, carrier: manifest.carrier, status: :shipped, manifest_id: nil)
     
     data = []
     shipment_ids = []
     
-    #begin
+    begin
       shipments.each do |s|
         next if s.courier_data.blank?
         
@@ -49,9 +54,10 @@ class Admin::Store::ManifestsController < Admin::BaseController
       
       # update shipments
       Shipment.where(id: shipment_ids).update_all(manifest_id: manifest.id)
-      #rescue => e
-      #flash[:error] = e.message
-      #end
+    rescue => e
+      Rails.logger.error e
+      flash[:error] = e.message
+    end
     
     redirect_back(fallback_location: admin_root_path)
   end
