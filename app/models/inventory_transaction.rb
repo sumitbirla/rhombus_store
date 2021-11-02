7cc48cdbc9bb9511660a3fefeb5d7b1bd8ee93d7
Sumit Birla <sbirla@tampahost.net>
2021-11-02 11:14:40 -0400

    SHOP-1310:  Speed up parsing of inventory files

--- app/models/inventory_transaction.rb
+++ app/models/inventory_transaction.rb
@@ -25,6 +25,7 @@ class InventoryTransaction < ActiveRecord::Base
   has_many :items, class_name: "InventoryItem", dependent: :destroy
   accepts_nested_attributes_for :items, reject_if: lambda { |x| x['sku'].blank? }, allow_destroy: true
 
+
   # Parse a text file containing inventory and return an InventoryTransaction instance
   #
   # @params [affiliate_id] the affiliate (fulfiller) who has uploaded this file
@@ -33,30 +34,29 @@ class InventoryTransaction < ActiveRecord::Base
   #
   def self.parse(affiliate_id, lines)
     aff = Affiliate.find(affiliate_id)
-    valid_item_numbers = AffiliateProduct.where(affiliate_id: aff.id).pluck(:item_number)
-    skus = Set.new(valid_item_numbers)
-    tran = InventoryTransaction.new(affiliate_id: aff.id, bulk_import: true)
+    tran = InventoryTransaction.new(affiliate_id: affiliate_id, bulk_import: true)
+    items = []
 
-    # If a file has more lines than expected, i.e. more than the products in database
-    # then log an error
-    if lines.length > skus.length + 50000
-      tran.errors.add(:base, "Too many records (#{lines.length}) in file, skipped parsing.")
-      return tran
-    end
-
+    # Parse each line for SKU and quantity and build a transaction object
     lines.each_with_index do |line, index|
       line.gsub!("\t\t", "\t") # delete any instances of multiple tabs
       row = line.split(/\t|,/).map(&:strip) # split by comma or tab
 
-      if !skus.include?(row[0])
+      if index <= 10 && !AffiliateProduct.exists?(affiliate_id: affiliate_id, item_number: row[0])
         # tran.errors.add(:base, "Line #{index}: Item number '#{row[0]}' not found.") unless index == 0
         next
       end
 
-      tran.items.build(sku: aff.code + "-" + row[0], quantity: row[1])
+      # if no valid SKU's are found in the first 10 rows,  assume file is bad and return
+      if index > 10 && items.empty?
+        tran.errors.add(:base, "Inventory doesn't appear to have valid item_numbers (first 10 rows analysed).")
+        return tran
+      end
+
+      items << InventoryItem.new(sku: aff.code + "-" + row[0], quantity: row[1])
     end
 
-    tran.errors.add(:base, "Inventory data has no valid items.") if tran.items.empty?
+    tran.items = items
     tran
   end
 
