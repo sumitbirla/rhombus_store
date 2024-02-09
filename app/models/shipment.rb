0018455fb57f4e1a4ae45bf4d3ec996cd80f7c91
Sumit Birla <sbirla@tampahost.net>
2024-02-09 10:17:32 -0500

    Removed inventory validation from Shipment model.

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -102,7 +102,7 @@ class Shipment < ActiveRecord::Base
   validates_presence_of :ship_from_company, :ship_from_street1, :ship_from_city, :ship_from_state, :ship_from_zip, :ship_from_country
   validates_presence_of :recipient_name, :recipient_street1, :recipient_city, :recipient_zip, :recipient_country, :fulfilled_by_id
   validates_presence_of :ship_date, if: Proc.new { |s| s.status.downcase == "shipped" }
-  validate :sufficient_inventory?, unless: Proc.new { |s| s.persisted? || s.skip_inventory }
+  #validate :sufficient_inventory?, unless: Proc.new { |s| s.persisted? || s.skip_inventory }
   #validates_presence_of :package_weight
 
   def skip_inventory
